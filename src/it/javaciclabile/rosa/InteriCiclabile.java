31a08f6c53dad611639f8dd67dfd0abc3069aa90
RosaCielo <ashyzan@gmail.com>
2024-06-01 17:01:36 +0200

    corrections

--- src/it/javaciclabile/rosa/InteriCiclabile.java
+++ src/it/javaciclabile/rosa/InteriCiclabile.java
@@ -3,8 +3,10 @@ package it.javaciclabile.rosa;
 
 public class InteriCiclabile {
 	
-	private int [] arrayInteri;
+	private int[] arrayInteri  = new int[100];
+
 	private int posizioneArray = 0;
+	private int numElementi = 0; // mantiene l'ultima posizione , equivale a this.arrayInteri.length
 
 //	Vogliamo realizzare una classe che contenga un elenco di interi e che ci permetta di ciclarli (dal primo all’ultimo).
 //	Dovrà offrire due metodi :
@@ -18,44 +20,72 @@ public class InteriCiclabile {
 	
 // costruttore per inizializzare l'array vuoto
 	
-	
 	public InteriCiclabile(int [] arrayInteriInput) {
-		 this.arrayInteri = arrayInteriInput ; // costruttore che prende come parametro un array di interi
-		
-		
+		 // costruttore che prende come parametro un array di interi
+//mp
+	System.out.println("numero posizioni array " + this.arrayInteri.length);
+		for(int a : arrayInteriInput ) {
+		this.arrayInteri[this.numElementi] = a;
+   	   	System.out.println("assegno indice: " + this.numElementi + " al valore " + a);
+		this.numElementi++;
+
+   	   }
 	}
 	
 //	public int[] InteriCiclabile() {
 //		return this.arrayInteri; // costruttore senza parametro
 //		
 //	}
+
+//mp
+	public int getNumeroElementi() {
+		return this.numElementi; 
+		
+	}
 	
-	public int getElementoSuccessivo( ) { //restituisce l'elemento successivo all'elemento invocato
-		
-		int[] b = new int[arrayInteri.length];
-		
-		for (int i=0; i<b.length; i++) {
-			
-			posizioneArray =  posizioneArray ++;
-			
-		}
-			
-			return b[posizioneArray + 1];
+
+
+	public int getElementoSuccessivo( ) { //restituisce l'elemento successivo all'elemento invocato, 
 
 		
-	
+//		int[] b = new int[arrayInteri.length];
+
+
+//		for (int i=0; i<b.length; i++) {
+			
+//			posizioneArray =  posizioneArray ++;
+			
+//		}
+//MP inizio da 0
+			posizioneArray =  posizioneArray ++;
+			return this.arrayInteri [posizioneArray -1];
 		
 	} 
 	
-//	public boolean hasAncoraElementi() {
-//		
-//	} //restituisce true se ci sono ancora elementi da restituire
-//	
-//	public void addElemento(int addElemento) {
-//		
-//		
-//		
-//	}
+	public boolean hasAncoraElementi() {
+
+		if (posizioneArray == (this.arrayInteri.length -1) ) {
+		return false;
+		} else 
+		return true;
+	} //restituisce true se ci sono ancora elementi da restituire
+
+
+	
+	public void addElemento(int elemento) {
+	
+	this.numElementi++;	
+	this.arrayInteri[numElementi] = elemento;
+	
+
+	for(int i=0; i<=this.numElementi; i++ ) {
+   	   	System.out.println("indice: " + i + " al valore " + this.arrayInteri[i]);
+		
+
+   	   }
+		
+
+}
 
 	// tostring
 	
--- src/it/javaciclabile/rosa/Main.java
+++ src/it/javaciclabile/rosa/Main.java
@@ -2,13 +2,37 @@ package it.javaciclabile.rosa;
 
 public class Main {
 
+
 	public static void main(String[] args) {
 		
 
-		InteriCiclabile prova = new InteriCiclabile({3 ,5, 88, 900, 223});
+	//	InteriCiclabile prova = new InteriCiclabile({3, 5, 88, 900, 223});
 		
-		prova.toString();
+
+	int[] age = {12, 4, 5};
+
+  	 // loop through the array
+ 	 // using for loop
+
+  	 System.out.println("Stampo contenuto dell'array:");
+  	 for(int a : age) {
+   	   System.out.println(a);
+   	   }
+	
+  	
+	InteriCiclabile prova = new InteriCiclabile(age);
+		
+	int numElementi= prova.getNumeroElementi();
+ 	System.out.println("Stampo n. totale elementi che è " + numElementi);
+
+ 	int elemento =18;
+	prova.addElemento(elemento);
+	
+	
+	prova.toString();
 
 	}
+	
+	
 
 }
